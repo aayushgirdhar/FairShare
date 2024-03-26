aaadf44eb197f34564713dfa9132a5a7d8d19204
AayushG02 <aayush.girdhar2@gmail.com>
2024-03-26 14:18:17 +0530

    minor changes

--- components/auth/LoginButton.tsx
+++ components/auth/LoginButton.tsx
@@ -1,23 +1,11 @@
 "use client";
 
 import { signIn } from "next-auth/react";
-// import { pink } from "@mui/material/colors";
 
 import GoogleIcon from "@mui/icons-material/Google";
 import GitHubIcon from "@mui/icons-material/GitHub";
 
-type data = {
-  email: string;
-  name: string;
-};
-
-export const LoginButton = ({
-  provider,
-  data,
-}: {
-  provider: string;
-  data?: data;
-}) => {
+export const LoginButton = ({ provider }: { provider: string }) => {
   return (
     <button
       className="border border-gray-300 rounded-lg w-36 h-10 flex items-center justify-center gap-4 shadow-sm"
