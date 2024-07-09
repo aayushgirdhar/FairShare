989c5a0a82b2aff2082491aa58071bfb1b89cc49
AayushG02 <aayush.girdhar2@gmail.com>
2024-07-09 20:52:58 +0530

    minor changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import Link from "next/link";
 import { getServerSession } from "next-auth";
 import { LogoutButton } from "@/components/auth/LogoutButton";
+import { options } from "./api/auth/[...nextauth]/options";
 
 export default async function Home() {
-  const session = await getServerSession();
+  const session = await getServerSession(options);
   if (!session) {
     return <Link href="/auth/login"> Login </Link>;
   }
